06a4f6055a2407087fdd0eb302ca8a879926fe23
michelesilva20 <michele.silva0511@gmail.com>
2023-08-05 12:26:11 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,87 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import imgDesktop from './images/hero-desktop.jpg';
+import logo from './images/logo.svg';
+import arrow from './images/icon-arrow.svg';
+import errorIcon from './images/icon-error.svg';
+import imgMobile from './images/hero-mobile.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+  };
+
+  const validateEmail = () => {
+    if (!email.includes('@')) {
+      setError('Please provide a valid email');
+      return false;
+    }
+    setError('');
+    return true;
+  };
+
+  const handleClick = () => {
+    const isValidEmail = validateEmail();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="info-coming">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="principal">
+            <img src={imgMobile} alt="" />
+          </div>
+          <div className='row'>
+            <div className="title">
+              <h1 className='we-are'>We're</h1>
+              <h1 className='coming-soon'>coming <br/> soon</h1>
+            </div>
+            <div className="text">
+              <p>Hello fellow shoppers! We're currently building our new <br/> fashion store. 
+              Add your email below to stay up-to-date with <br/> announcements and our launch deals.</p>
+            </div>
+            <form action="" noValidate>
+              <div className="input">
+                <div className='container-form'>
+                <input type="email" placeholder='Email Address' 
+                  required 
+                  value={email} 
+                  onChange={handleInputChange} 
+                  className={error ? 'error-border ' : ''}
+                />
+               {error ?  <div className='error-icon'>
+                  <img src={errorIcon} alt="erro" />
+                </div> : ''}
+                <div className="error-message">{error}</div>
+                </div>
+                <div className="button">
+                  <button type='button' onClick={handleClick}>
+                    <img src={arrow} alt="" />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="img-principal">
+          <img src={imgDesktop} alt="imagem principal" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        {/*<footer>
+          <p class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+          </p>
+        </footer>*/}
     </>
   )
 }
